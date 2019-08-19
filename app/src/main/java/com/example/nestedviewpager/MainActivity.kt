5359be220ee75c5066a9a222cad56214bf74b356
Michael Prince Ryan J Wellen <mike_wellen@bcbsil.com>
2019-08-19 14:16:59 -0500

    initial commit

---
+++ app/src/main/java/com/example/nestedviewpager/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.nestedviewpager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentStatePagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        view_pager.adapter = object: FragmentStatePagerAdapter(supportFragmentManager) {
+
+            val views = listOf(
+                ViewPagerFragment.newInstance(),
+                ViewPagerFragment.newInstance(),
+                ViewPagerFragment.newInstance()
+            )
+
+            override fun getCount(): Int = views.count()
+
+            override fun getPageTitle(position: Int): CharSequence = position.toString()
+
+            override fun getItem(position: Int): Fragment = views[position]
+
+        }
+
+        tab_layout.setupWithViewPager(view_pager)
+
+    }
+}
